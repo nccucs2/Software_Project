d019c01a32b73ddaaabfd5d8392000680df9d286
James0828 <hippo70328@gmail.com>
2018-06-21 16:37:47 +0800

    models change

--- mainsite/admin.py
+++ mainsite/admin.py
@@ -2,10 +2,11 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
-from .models import student_info,course,course_grade,gpa
+from .models import student_info,course,course_grade,gpa,personal_info
 # Register your models here.
 
 admin.site.register(student_info)
 admin.site.register(course)
 admin.site.register(course_grade)
 admin.site.register(gpa)
+admin.site.register(personal_info)
