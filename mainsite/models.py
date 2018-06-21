fe3f05fa615fdd52de3279199fa4a9ef39ad5c1d
vancelot7789 <a0952072007@gmail.com>
2018-06-21 22:17:54 +0800

    course data

--- mainsite/models.py
+++ mainsite/models.py
@@ -11,15 +11,16 @@ class student_info(models.Model):
 
 class course(models.Model):
     AcademicYear = models.IntegerField()
-    Semester  = models.IntegerField()
+    Semester  = models.IntegerField(null=True)
     course_name = models.TextField()
-    credit = models.DecimalField(max_digits=2,decimal_places=1)
+    credit = models.CharField(max_length=5,blank=True)
     teacher = models.TextField()
+#    object = models.CharField(max_length=3,blank=True)
     course_code = models.CharField(max_length=50,primary_key=True)
     course_category = models.CharField(max_length=5,blank=True)
-    department = models.TextField(blank=True)
-    course_type = models.CharField(max_length=5,blank=True)
-    general_type = models.CharField(max_length=5,blank=True)
+    deparment=models.TextField(blank=True)
+    course_type=models.CharField(max_length=5,blank=True)
+    general_type=models.CharField(max_length=5,blank=True)
     def __str__(self):
         return self.teacher + " " + self.course_name
 
@@ -47,3 +48,42 @@ class personal_info(models.Model):
     english_name = models.CharField(max_length=20)
     def __str__(self):
         return self.user
+
+
+# a = course.objects.all()
+# #a.delete()
+f = open('/Users/byron/Desktop/1062_courses_all_cleaned_22.csv','r',encoding='utf-16', errors='ignore')
+list=[]
+counter = 0
+number = 0
+"""
+for line in f.readlines():
+     if counter ==0:
+         counter+=1
+         continue;
+     else:
+         AcademicYear=line.strip().split(',')[1]
+         Semester=(line.strip().split(',')[3])
+
+         try:
+             print(int(Semester))
+             number=int(Semester)
+         except:
+             number=0
+             print(0)
+         course_name=line.strip().split(',')[4]
+
+         credit=line.strip().split(',')[7]
+         teacher=line.strip().split(',')[6]
+         course_code=line.strip().split(',')[2]
+         deparment=line.strip().split(',')[12]
+         course_type=line.strip().split(',')[13]
+         general_type=line.strip().split(',')[15]
+
+         #course.objects.create(AcademicYear=AcademicYear,Semester=number,course_name=course_name,credit=(credit),teacher=teacher,course_code=course_code,deparment=deparment,course_type=course_type,general_type=general_type)
+         print(AcademicYear,Semester,course_name,credit,teacher,course_code,deparment,course_type,general_type)
+         # print(type( Semester))
+         counter+=1
+         # if counter==10:
+         #     break
+"""
